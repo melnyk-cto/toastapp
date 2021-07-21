9ada767c7811b2b6655ea18f2e0c7a9b38d24bcb
Mykola Melnyk <melnyk.cto.dev@gmail.com>
2021-07-21 17:28:37 +0300

    fix: redirect

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,28 +2,26 @@
 import React from 'react';
 
 // library
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 // components
-import { Home} from "../pages";
+import { Home } from "../pages";
 
 import { Footer, Header } from "../common";
-import { routes } from './routes';
 
 // assets
 import styles from './App.module.scss';
 
 function App() {
-    return (
-        <div className={styles.app}>
-            <Header />
-            <Switch>
-                <Redirect exact from='/' to={routes.home} />
-                <Route path={routes.home} component={Home} />
-            </Switch>
-            <Footer />
-        </div>
-    );
+  return (
+    <div className={styles.app}>
+      <Header />
+      <Switch>
+        <Route path='/' component={Home} />
+      </Switch>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
