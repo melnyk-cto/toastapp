ea2b4b403312197e50b1e020de1e4441270032c3
Mykola Melnyk <melnyk.cto.dev@gmail.com>
2021-08-04 19:11:45 +0300

    fix: calendar

--- src/components/pages/event/EventCalendar/EventCalendar.jsx
+++ src/components/pages/event/EventCalendar/EventCalendar.jsx
@@ -11,17 +11,35 @@ import { routes } from "../../../App/routes";
 import styles from "./EventCalendar.module.scss";
 import { ReactComponent as BackSvg } from "../../../../assets/images/icons/arrow-back.svg";
 import { ReactComponent as MoreSvg } from "../../../../assets/images/icons/more-horizontal.svg";
-import avatar1 from "../../../../assets/images/events/avatar1.png";
-import avatar2 from "../../../../assets/images/events/avatar2.png";
-import avatar3 from "../../../../assets/images/events/avatar3.png";
+import avatar1 from "../../../../assets/images/events/calendar/avatar-1.png";
+import avatar2 from "../../../../assets/images/events/calendar/avatar-2.png";
+import avatar3 from "../../../../assets/images/events/calendar/avatar-3.png";
+import avatar4 from "../../../../assets/images/events/calendar/avatar-4.png";
+import avatar5 from "../../../../assets/images/events/calendar/avatar-5.png";
+import avatar6 from "../../../../assets/images/events/calendar/avatar-6.png";
+import avatar7 from "../../../../assets/images/events/calendar/avatar-7.png";
+import avatar8 from "../../../../assets/images/events/calendar/avatar-8.png";
+import avatar9 from "../../../../assets/images/events/calendar/avatar-9.png";
+import avatar10 from "../../../../assets/images/events/calendar/avatar-10.png";
+import avatar11 from "../../../../assets/images/events/calendar/avatar-11.png";
+import avatar12 from "../../../../assets/images/events/calendar/avatar-12.png";
 
 
 const weeks = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const events = [
-  {avatar: avatar1, month: 'September', day: 2},
-  {avatar: avatar2, month: 'September', day: 7},
-  {avatar: avatar3, month: 'September', day: 10}
+  {id: 0, avatar: avatar1, month: 'September', day: 2},
+  {id: 1, avatar: avatar2, month: 'September', day: 7},
+  {id: 2, avatar: avatar3, month: 'September', day: 10},
+  {id: 3, avatar: avatar4, month: 'September', day: 11},
+  {id: 4, avatar: avatar5, month: 'September', day: 15},
+  {id: 5, avatar: avatar6, month: 'September', day: 17},
+  {id: 6, avatar: avatar7, month: 'September', day: 18},
+  {id: 7, avatar: avatar8, month: 'September', day: 20},
+  {id: 8, avatar: avatar9, month: 'September', day: 21},
+  {id: 9, avatar: avatar10, month: 'September', day: 24},
+  {id: 10, avatar: avatar11, month: 'September', day: 25},
+  {id: 11, avatar: avatar12, month: 'September', day: 27},
 ]
 export const EventCalendar = () => {
 
@@ -30,10 +48,10 @@ export const EventCalendar = () => {
 
     const date = new Date();
     let year = date.getFullYear();
-    let month = date.getMonth() + 1;
+    let month = date.getMonth();
 
     const config = {
-      show: 3,
+      show: 2,
     }
 
     const showCalendar = (year, month) => {
@@ -70,6 +88,7 @@ export const EventCalendar = () => {
         calendarHtml += '<td class="title">' + weeks[i] + '</td>';
       }
 
+      let count = 0;
       for (let w = 0; w < 6; w++) {
         calendarHtml += '<tr>';
 
@@ -82,7 +101,12 @@ export const EventCalendar = () => {
             calendarHtml += `<td class='is-disabled'>${num}</td>`
             dayCount++;
           } else {
-            calendarHtml += '<td>' + dayCount + '</td>';
+            if (events.filter(event => event.day === dayCount).length !== 0) {
+              count = count + 1;
+              calendarHtml += `<td class='event'><img src='${events[count - 1].avatar}' alt=''><span>${dayCount}</span></td>`;
+            } else {
+              calendarHtml += '<td>' + dayCount + '</td>';
+            }
             dayCount++;
           }
         }
